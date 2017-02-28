6a4f9896f5a6d76759573e35254164b27845d2f4
Limon <fengninglong@gmail.com>
2017-02-28 17:27:21 +0800

    refine demo

--- Modules/Lego/Lego/FirstViewController.swift
+++ Modules/Lego/Lego/FirstViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class FirstViewController: UIViewController {
 
+    private let disposeBag = DisposeBag()
+
+    @IBOutlet private weak var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        loginButton.rx.tap
+            .throttle(0.3, scheduler: MainScheduler.instance)
+            .subscribe(onNext: { _ in
+                (UIApplication.shared.delegate as? AppDelegate)?.startDoorStory()
+            })
+            .addDisposableTo(disposeBag)
+    }
+
+    deinit {
+        print("FirstViewController Deinit")
     }
 }
 
