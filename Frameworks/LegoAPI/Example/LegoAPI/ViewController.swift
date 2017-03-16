cc913a91341be6f758b88916c398042ff0ea8d91
Limon <fengninglong@gmail.com>
2017-03-16 23:52:37 +0800

    update podfile source

--- Frameworks/LegoAPI/Example/LegoAPI/ViewController.swift
+++ Frameworks/LegoAPI/Example/LegoAPI/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import RxSwift
 import LegoProvider
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userProvider = LegoProvider<UserAPI>()
+        let provider = LegoProvider<UserAPI>()
+
+        provider.request(.login([:])) { (_) in
+
+        }
+//        provider.request(.login([:])).subscribe { event in
+//            switch event {
+//            case let .next(response):
+//                image = UIImage(data: response.data)
+//            case let .error(error):
+//                print(error)
+//            default:
+//                break
+//            }
+//        }
     }
 
 }
