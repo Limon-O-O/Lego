add8fca6719c4d4f08d1131223f211b608111992
Limon <fengninglong@gmail.com>
2017-03-16 11:13:24 +0800

    update Test template

--- ConfigPrivatePod/pod-template/setup/test_examples/quick.swift
+++ ConfigPrivatePod/pod-template/setup/test_examples/quick.swift
@@ -2,7 +2,6 @@
 
 import Quick
 import Nimble
-import PROJECT
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
--- ConfigPrivatePod/pod-template/setup/test_examples/xctest.swift
+++ ConfigPrivatePod/pod-template/setup/test_examples/xctest.swift
@@ -1,6 +1,5 @@
 import UIKit
 import XCTest
-import PROJECT
 
 class Tests: XCTestCase {
     
